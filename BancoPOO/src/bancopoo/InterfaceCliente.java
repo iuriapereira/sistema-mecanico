db8504dde3e9802686a7693eef3374f18e8e2f7f
Iuri Pereira <98133646+iuriapereira@users.noreply.github.com>
2023-06-07 00:09:53 -0300

    Merge branch 'main' of https://github.com/iuriapereira/sistema-mecanico

--- BancoPOO/src/bancopoo/InterfaceCliente.java
+++ BancoPOO/src/bancopoo/InterfaceCliente.java
@@ -125,7 +125,7 @@ class InterfaceCliente {
         DefaultTableModel model = new DefaultTableModel(data, columnNames) {
             @Override
             public boolean isCellEditable(int row, int column) {
-                return false;
+                return true;
             }
         };
 
